ffa25704778b96f8f1f938ba9357c3dbda57762d
Kunal <kunal.jain@ekamtek.com>
2021-11-26 19:55:29 +0530

    changes in gallery.

--- config/database.js
+++ config/database.js
@@ -4,8 +4,9 @@ const moongoose = require('moongoose')
 const db = `mongodb://localhost:27017/gallery?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false`
 
 
-const ConnectDB = async ()=>{
-    await moongoose.connect(db)
+const ConnectDB = async () => {
+    await moongoose.connect(db, { useUnifiedTopology: true, useNewUrlParser: true }).
+        catch((e)=>{console.log(error)})
     console.log(`database connected.`);
 }
 
