0504eb123c770056c5c753a51d55e219b38df99f
Kunal <kunal.jain@ekamtek.com>
2021-11-26 21:21:50 +0530

    addded chnages in add data .

--- routes/index.js
+++ routes/index.js
@@ -2,24 +2,36 @@ const express = require('express');
 const router = express.Router();
 const UserSchmea = require('../Model/Schema')
 
-router.get('/',async(req,res)=>{
+router.get('/', async (req, res) => {
     const user = await UserSchmea.find();
     res.send(user);
 })
 
-router.post('/res',async(req,res)=>{
-    let user = new UserSchmea(req.body);
+router.post('/res', async (req, res) => {
+
+    const image = req.files.img;
+    const encode_img = image.toString('base64');
+    // const final_img = {
+    //     contentType: req.file.mimetype,
+    //     image: new Buffer(encode_img, 'base64')
+    // };
+
+    let user = new UserSchmea({
+        name: req.body.name,
+        img: encode_img
+    });
+
     await user.save();
     res.send(user);
 })
 
-router.put('/res/:id',async(req,res)=>{
-    const {id} = req.params;
-    const user =  await UserSchmea.findByIdAndUpdate(id,req.body);
+router.put('/res/:id', async (req, res) => {
+    const { id } = req.params;
+    const user = await UserSchmea.findByIdAndUpdate(id, req.body);
     res.send(req.body);
 })
 
-router.delete('/res/:id',async(req,res)=>{
+router.delete('/res/:id', async (req, res) => {
     const user_id = req.params.id;
     const user = await UserSchmea.findByIdAndDelete(user_id);
     res.send(user);
