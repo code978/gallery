0355e0abbffbe20712e940a43f9206f96f3e9857
Kunal <kunal.jain@ekamtek.com>
2021-11-27 19:18:56 +0530

    added url for mongodb in gallery.

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+// var url = "mongodb://localhost:27017/mydb";
 const url = `mongodb+srv://test:test@cluster0.snafy.mongodb.net/gallery?retryWrites=true&w=majority`
 
 const ConnectDB = async () => {
